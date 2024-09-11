5183639844a2d0ac98469ac6d3d6433cb5571bbb
BoakyePrincella <princellaboakye2003@gmail.com>
2024-09-11 22:26:09 +0000

    Created Landing page

--- medforecast/bin/activate.fish
+++ medforecast/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/grandkojo/GITHUB/MedForecast/medforecast"
+set -gx VIRTUAL_ENV "/home/princellab/GITHUB/MedForecast/medforecast"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
